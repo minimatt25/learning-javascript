bd48434f048ea104119bd3abda4f9bfcbefb78c6
Matt Adam <95365935+minimatt25@users.noreply.github.com>
2024-07-22 15:42:07 +0100

    practice basket system

--- main jscript.js
+++ main jscript.js
@@ -1,3 +1,6 @@
+let basketNum = 0;
+let basketVal = 0;
+
 function changeText(){
     document.getElementById("buttonChange").innerHTML = "the button worked";
 }
@@ -15,4 +18,31 @@ function outputUP(){
     const username = usernameInput.value;
     const password = passwordInput.value;
     document.getElementById("userPass").innerHTML = "your username is " + username + " and your password is " + password;
+}
+
+function addToB(clickedID){
+
+    //items in the basket
+    basketNum+=1;
+    console.log(basketNum);
+    if(basketNum!=1){
+        document.getElementById("items").innerHTML = "there are " + basketNum + " items in your basket.";
+    } else{
+        document.getElementById("items").innerHTML = "there is " + basketNum + " item in your basket.";
+    }
+
+    //price of items overall
+    document.getElementById("ID").innerHTML = "ID is " + clickedID;
+    
+    if(clickedID == "Invincible c1" || clickedID == "Invincible c2" || clickedID == "Invincible c3"){
+        basketVal+=39.99;
+    } else if(clickedID == "Secret invasion" || clickedID == "Civil war" || clickedID == "long halloween"){
+        basketVal+=20;
+    } else if(clickedID == "year one"){
+        basketVal+=10;
+    } else if(clickedID == "Watchmen"){
+        basketVal+=25;
+    }
+    console.log(basketVal);
+    document.getElementById("cost").innerHTML = "Price = £" + (Math.round(basketVal * 100) / 100).toFixed(2);
 }
